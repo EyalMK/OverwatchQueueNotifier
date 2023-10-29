4e41980603c114858c99060efb04e0de2d233c44
Eyal Maklada <wezkc21@gmail.com>
2023-10-30 01:54:45 +0200

    1. Edited Discord Bot messages to mention user, to force the notification.

--- server/DiscordBot.py
+++ server/DiscordBot.py
@@ -114,10 +114,10 @@ class DiscordBot(discord.Client):
             print('Error - user not found. Make sure to provide the correct user id.')
 
     async def send_user_reminder(self, user_id):
-        await self.send_user_message(user_id, "Reminder - Invite your friend/s :)")
+        await self.send_user_message(user_id, f'<@{user_id}> Reminder - Invite your friend/s :)')
 
     async def notify_user_game_found(self, user_id):
-        await self.send_user_message(user_id, "Your match has been found!")
+        await self.send_user_message(user_id, f'<@{user_id}> Your match has been found!')
 
     async def inform_user_select_hero_invalid(self, user_id):
         await self.send_user_message(user_id, "The hero you requested does not exist. Invalid input.")
