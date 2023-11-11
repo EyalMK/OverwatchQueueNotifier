5bcd185b1961ee2d39f3e56d565878fb35de9832
Eyal Maklada <wezkc21@gmail.com>
2023-11-11 12:33:46 +0200

    1. Added more API routes 2. Workflow edits

--- tools/api.py
+++ tools/api.py
@@ -2,6 +2,249 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+region_resolution_options = {
+    "queue_region_big_1920x1080": {
+        "x": 750,
+        "y": 0,
+        "width": 425,
+        "height": 139
+    },
+    "queue_region_big_2560x1440": {
+        "x": 998,
+        "y": 0,
+        "width": 566,
+        "height": 186
+    },
+    "queue_region_small_1920x1080": {
+        "x": 750,
+        "y": 0,
+        "width": 425,
+        "height": 40
+    },
+    "queue_region_small_2560x1440": {
+        "x": 998,
+        "y": 0,
+        "width": 566,
+        "height": 53
+    },
+    "queue_region_top_left_1920x1080": {
+        "x": 100,
+        "y": 20,
+        "width": 200,
+        "height": 40
+    },
+    "queue_region_top_left_2560x1440": {
+        "x": 133.33,
+        "y": 26.67,
+        "width": 266.67,
+        "height": 53.33
+    },
+    "queue_pop_region_1920x1080": {
+        "x": 770,
+        "y": 0,
+        "width": 375,
+        "height": 100
+    },
+    "queue_pop_region_2560x1440": {
+        "x": 1024,
+        "y": 0,
+        "width": 500,
+        "height": 133.33
+    },
+    "menu_region_1920x1080": {
+        "x": 850,
+        "y": 0,
+        "width": 200,
+        "height": 500
+    },
+    "menu_region_2560x1440": {
+        "x": 1133.33,
+        "y": 0,
+        "width": 266.67,
+        "height": 666.67
+    },
+    "full_menu_region_1920x1080": {
+        "x": 870,
+        "y": 150,
+        "width": 320,
+        "height": 800
+    },
+    "full_menu_region_2560x1440": {
+        "x": 1168,
+        "y": 200,
+        "width": 373.33,
+        "height": 1066.67
+    },
+    "hero_change_button_region_1920x1080": {
+        "x": 850,
+        "y": 950,
+        "width": 250,
+        "height": 75
+    },
+    "hero_change_button_region_2560x1440": {
+        "x": 1133.33,
+        "y": 1066.67,
+        "width": 333.33,
+        "height": 100
+    },
+    "assemble_team_region_1920x1080": {
+        "x": 780,
+        "y": 510,
+        "width": 350,
+        "height": 60
+    },
+    "assemble_team_region_2560x1440": {
+        "x": 1040,
+        "y": 680,
+        "width": 466,
+        "height": 80
+    },
+    "available_selection_1920x1080": {
+        "x": 875,
+        "y": 962,
+        "width": 170,
+        "height": 55
+    },
+    "available_selection_2560x1440": {
+        "x": 1166,
+        "y": 1282,
+        "width": 226,
+        "height": 73
+    },
+    "hero_select_details_region_1920x1080": {
+        "x": 20,
+        "y": 1000,
+        "width": 400,
+        "height": 40
+    },
+    "hero_select_details_region_2560x1440": {
+        "x": 30.67,
+        "y": 1133.33,
+        "width": 533.33,
+        "height": 53.33
+    },
+    "enter_game_region_1920x1080": {
+        "x": 780,
+        "y": 505,
+        "width": 320,
+        "height": 50
+    },
+    "enter_game_region_2560x1440": {
+        "x": 936.67,
+        "y": 606.67,
+        "width": 426.67,
+        "height": 66.67
+    },
+    "obj_mode_region_1920x1080": {
+        "x": 165,
+        "y": 125,
+        "width": 165,
+        "height": 60,
+    },
+    "obj_mode_region_2560x1440": {
+        "x": 220,
+        "y": 167,
+        "width": 220,
+        "height": 80,
+    },
+    "map_name_region_1920x1080": {
+        "x": 160,
+        "y": 185,
+        "width": 400,
+        "height": 40,
+    },
+    "map_name_region_2560x1440": {
+        "x": 213,
+        "y": 246,
+        "width": 533,
+        "height": 53,
+    },
+    "play_mode_region_1920x1080": {
+        "x": 170,
+        "y": 60,
+        "width": 500,
+        "height": 150
+    },
+    "play_mode_region_2560x1440": {
+        "x": 226,
+        "y": 80,
+        "width": 666,
+        "height": 200
+    },
+    "potm_title_region_1920x1080": {
+        "x": 40,
+        "y": 20,
+        "width": 280,
+        "height": 65
+    },
+    "potm_title_region_2560x1440": {
+        "x": 53.33,
+        "y": 26.67,
+        "width": 373.33,
+        "height": 88.89
+    },
+    "progression_screen_title_region_1920x1080": {
+        "x": 1540,
+        "y": 100,
+        "width": 250,
+        "height": 25
+    },
+    "progression_screen_title_region_2560x1440": {
+        "x": 1920,
+        "y": 133.33,
+        "width": 333.33,
+        "height": 37.04
+    },
+    "comp_progress_cards_region_1920x1080": {
+        "x": 700,
+        "y": 150,
+        "width": 550,
+        "height": 75
+    },
+    "comp_progress_cards_region_2560x1440": {
+        "x": 853.33,
+        "y": 300,
+        "width": 733.33,
+        "height": 111.11
+    },
+    "menu_play_sign_1920x1080": {
+        "x": 50,
+        "y": 50,
+        "width": 450,
+        "height": 175
+    },
+    "menu_play_sign_2560x1440": {
+        "x": 67,
+        "y": 67,
+        "width": 600,
+        "height": 233
+    },
+    "landing_menu_region_1920x1080": {
+        "x": 1600,
+        "y": 1000,
+        "width": 150,
+        "height": 50
+    },
+    "landing_menu_region_2560x1440": {
+        "x": 2133,
+        "y": 1333,
+        "width": 200,
+        "height": 66
+    }
+}
+
+coordinates_resolution_options = {
+    "select_hero_button_1920x1080": {
+        "x": 950,
+        "y": 970
+    },
+    "select_hero_button_2560x1440": {
+        "x": 1266,
+        "y": 1293
+    },
+}
+
+
 # Heroes
 tanks = {
     "D.Va": ["D.Va", "d.va", "dva", "Dva"],
@@ -101,7 +344,7 @@ heroes_coordinates_1920x1080 = {
     "Zenyatta": [1660, 890]
 }
 
-heroes_coordinates_2560x1440 = {hero: [int(coord[0] * 1.3333), int(coord[1] * 1.3333)] for hero, coord in
+heroes_coordinates_2560x1440 = {hero: [int(coord[0] * 4 / 3), int(coord[1] * 4 / 3)] for hero, coord in
                                 heroes_coordinates_1920x1080.items()}
 
 maps = {
@@ -140,7 +383,7 @@ maps = {
 
     # Push
     "Colosseo": ["colosseo", "colo", "sseo"],
-    "Esperanca": ["esperanca", "esper", "anca"],
+    "Esperanca": ["esperanca", "esper", "anca", "esperanga"],
     "New Queen Street": ["new queen street", "new queen", "queen", "street"],
 
     # Flashpoint
@@ -186,6 +429,16 @@ def get_heroes_coordinates_1920x1080():
 @app.route('/api/heroes/coordinates_2560x1440', methods=['GET'])
 def get_heroes_coordinates_2560x1440():
     return jsonify(heroes_coordinates_2560x1440)
+
+
+@app.route('/api/screen/coordinates_options', methods=['GET'])
+def get_coordinates_resolution_options():
+    return jsonify(coordinates_resolution_options)
+
+
+@app.route('/api/screen/region_options', methods=['GET'])
+def get_region_resolution_options():
+    return jsonify(region_resolution_options)
 
 
 if __name__ == '__main__':
